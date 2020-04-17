22fdb286aea0b0eebde6e582217518a29d7e54c1
is-lgarcia <luis10.g@hotmail.com>
2020-04-16 23:55:46 -0600

    creación de archivos (pantallas) de acuerdo al Mockoup

---
+++ app/src/main/java/com/luisg/conf/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,13 @@
+package com.luisg.conf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.luisg.conf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
